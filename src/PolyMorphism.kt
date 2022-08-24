c8f50959c0185ca756ca69cbbf5214bcc5c0b828
swapnil20711 <swapnil.bhojwanI@gmail.com>
2022-08-25 00:59:30 +0530

    Abstraction and interface done

--- src/PolyMorphism.kt
+++ src/PolyMorphism.kt
@@ -6,6 +6,7 @@ fun main() {
     val shapeTriangle: Shape = Triangle(5.0, 10.0)
     val list = arrayListOf<Shape>(shapeSquare, shapeCircle, shapeTriangle)
     calculateAreas(list)
+    dragObjects(list)
 }
 
 fun calculateAreas(list: List<Shape>) {
@@ -14,15 +15,26 @@ fun calculateAreas(list: List<Shape>) {
     }
 }
 
-open class Shape() {
-    open fun area(): Double {
-        return 0.0
+fun dragObjects(list: List<Draggable>) {
+    for (obj in list) {
+        obj.drag()
     }
+}
+
+abstract class Shape() : Draggable {
+    /*
+    * Child class will define it's property
+    * */
+    abstract fun area(): Double
 }
 
 open class Circle(private val radius: Double) : Shape() {
     override fun area(): Double {
         return "%.2f".format(Math.PI * radius * radius).toDouble()
+    }
+
+    override fun drag() {
+        println("Circle is dragging")
     }
 }
 
@@ -30,10 +42,25 @@ open class Square(val side: Double) : Shape() {
     override fun area(): Double {
         return side * side
     }
+
+    override fun drag() {
+        println("Square is dragging")
+    }
 }
 
 open class Triangle(val base: Double, val height: Double) : Shape() {
     override fun area(): Double {
         return 0.5 * base * height
     }
+
+    override fun drag() {
+        println("Triangle is dragging")
+    }
+}
+
+/*
+* Group objects on basis of behaviour like dragging is common behaviour among classes here
+* */
+interface Draggable {
+    fun drag()
 }
